421787228404b532ea28e98877e45eadb3fa644e
Georgi Kolev <georgipetrovkolev1998@gmail.com>
2024-06-24 16:34:26 +0300

    terraform state files and project add

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "google" {
+  credentials = file("tf-key-state.json")
+  project = var.project
+  region = var.region
+  zone = var.zone
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "mybucket-georgik-kolev1998-tf-state-123"
+    prefix = "terraform-state"
+    credentials = "tf-key-state.json"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  default = "gcp-new-427413"
+}
+
+variable "region" {
+    default = "europe-west12"
+}
+
+variable "zone" {
+    default = "europe-west12-a"
+}
